5f4de48f9fee7bea3c49c45c8e4201260b44062d
Ivan Chan <ivan.chanzhengyang@yahoo.com.sg>
2023-11-09 09:45:20 +0800

    Updating files

--- streamlit_app_trope_scanner.py
+++ streamlit_app_trope_scanner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 #importing scripts
@@ -11,26 +11,34 @@ from bs4 import BeautifulSoup
 import requests
 
 
-# In[2]:
+# In[4]:
 
 
 # Function to scrape TV Tropes for trope information
 def scrape_tvtropes(trope_name):
-    url = f"https://tvtropes.org/pmwiki/pmwiki.php/Main/{trope_name}"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    trope_description = soup.find("div", class_="page-content").text
-    return trope_description
+    base_url = "https://tvtropes.org/pmwiki/pmwiki.php/Main/"
+    url = base_url + trope_name
+
+    try:
+        page = requests.get(url)
+        page.raise_for_status()
+        soup = BeautifulSoup(page.content, "html.parser")
+        trope_description = soup.find("div", class_="page-content").text
+        return trope_description
+    except requests.exceptions.HTTPError as e:
+        st.error(f"HTTP error: {e}")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 # Streamlit app
 st.title("Trope Scanner")
 
 trope_name = st.text_input("Enter a trope name:")
 if trope_name:
-    try:
-        trope_description = scrape_tvtropes(trope_name)
+    trope_description = scrape_tvtropes(trope_name)
+    if trope_description:
         st.write("Trope Description:")
         st.write(trope_description)
-    except:
-        st.error("Trope not found. Please enter a valid trope name.")
+    else:
+        st.warning("Trope not found. Please enter a valid trope name.")
 
